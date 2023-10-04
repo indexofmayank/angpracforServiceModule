1262473eef2929802a51b5c0f704b71ce9c8f0b2
indexofmayank <maimayanmanu@gmail.com>
2023-10-04 15:37:52 +0530

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  posts: any;
   title = 'pracangfour';
+
+  constructor(private service: PostService) {}
+
+  ngOnInit(): void {
+    this.service.getPosts()
+    .subscribe(response => {
+      this.posts = response;
+    });
+  }
 }
